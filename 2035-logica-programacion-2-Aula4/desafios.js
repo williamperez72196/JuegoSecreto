9f4bbbd43e77e52ae538b0feb4b1f6a7a87e9ce0
WILLIAM ALBERTO PEREZ MAURY <william72196@gmail.com>
2024-01-21 14:05:59 -0500

    Version final juego Numero Secreto

---
+++ 2035-logica-programacion-2-Aula4/desafios.js
@@ -0,0 +1,83 @@
+// 1. Crea una lista vacía con el nombre listaGenerica.
+let listaGenerica = [];
+
+// 2. Crea una lista de lenguajes de programación llamada lenguajesDeProgramacion.
+let lenguagesDeProgramacion = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+// 3. Agrega a la lista idiomasDeProgramacion los siguientes elementos.
+lenguagesDeProgramacion.push('Java', 'Ruby', 'GoLang');
+
+// 4. Crea una función que muestre en la consola todos los elementos de la lista idiomasDeProgramación por separado.
+function mostrarLenguagesSeparadamente() {
+  for (let i = 0; i < lenguagesDeProgramacion.length; i++) {
+    console.log(lenguagesDeProgramacion[i]);
+  }
+}
+
+mostrarLenguagesSeparadamente();
+
+// 5. Crea una función que muestre en la consola todos los elementos de la lista idiomasDeProgramación de manera inversa.
+function mostrarLenguagesReversoSeparadamente() {
+  for (let i = lenguagesDeProgramacion.length - 1; i >= 0; i--) {
+    console.log(lenguagesDeProgramacion[i]);
+  }
+}
+
+mostrarLenguagesReversoSeparadamente();
+
+// 6. Crea una función que calcula la media de los elementos en una lista de números.
+function calcularMedia(lista) {
+  let suma = 0;
+  for (let i = 0; i < lista.length; i++) {
+    suma += lista[i];
+  }
+  return suma / lista.length;
+}
+
+let numeros = [10, 20, 30, 40, 50];
+let media = calcularMedia(numeros);
+console.log('Média:', media);
+
+// 7. Crea una función que muestre en la consola el número mayor y menor en una lista.
+function encontrarMayorMenor(lista) {
+  let mayor = lista[0];
+  let menor = lista[0];
+
+  for (let i = 1; i < lista.length; i++) {
+    if (lista[i] > mayor) {
+      mayor = lista[i];
+    }
+    if (lista[i] < menor) {
+      menor = lista[i];
+    }
+  }
+
+  console.log('Mayor:', mayor);
+  console.log('Menor:', menor);
+}
+
+let numeros = [15, 8, 25, 5, 12];
+encontrarMayorMenor(numeros);
+
+// 8. Crea una función que retorna la suma de todos los elementos en una lista.
+function calcularSuma(lista) {
+  let suma = 0;
+  for (let i = 0; i < lista.length; i++) {
+    suma += lista[i];
+  }
+  return suma;
+}
+
+let numeros = [15, 8, 25, 5, 12];
+let suma = calcularSuma(numeros);
+console.log('Suma:', suma);
+
+// 9. Crea una función que recibe una lista como parámetro y retorna el índice de un elemento también pasado como parámetro. Si ese elemento no existe en la lista, retorna -1.
+function encontrarIndiceElemento(lista, elemento) {
+  for (let i = 0; i < lista.length; i++) {
+    if (lista[i] === elemento) {
+      return i; // Retorna el índice del elemento encontrado
+    }
+  }
+  return -1; // Retorna -1 si el elemento no se encuentra en la lista
+}
